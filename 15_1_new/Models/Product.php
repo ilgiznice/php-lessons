61063cf0a2f2cd4490e189309873ffe7511fc970
Ilgiz Mavlyaviev <ilgiz.personal@gmail.com>
2017-10-12 18:19:20 +0300

    [remove] 15_1_new

--- 15_1_new/Models/Product.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Models;
-
-class Product extends \Database
-{
-    public $table = 'products';
-    public $allowed = ['title', 'description', 'price'];
-}
--- 15_1_new/Models/Review.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Models;
-
-class Review extends \Database
-{
-    public $table = 'reviews';
-    public $allowed = ['name', 'phone', 'text'];
-}
